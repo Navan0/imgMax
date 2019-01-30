583bfd43f720b8a74747da4f5341dad83df6a766
Your Name <nktclt@gmail.com>
2019-01-30 12:22:08 +0530

    update repo

--- highRes.py
+++ highRes.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import numpy as np
-img = np.asarray(Image.open('/home/navaneeth/work/test/mine.jpg'))
-img.setflags(write=1)
+img = np.asarray(Image.open('/home/navaneeth/work/test/mine.jpg')) # image to numpy array
+img.setflags(write=1) # change read-only to write
 img2 = np.asarray(Image.open('/home/navaneeth/work/test/test2.png'))
 img2.setflags(write=1)
 img2[1,0] = img[1,0]
