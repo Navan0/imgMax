577cf549c4a6045420ddb5539ae5c474f2a3c48a
Your Name <nktclt@gmail.com>
2019-01-31 12:24:48 +0530

    update repo

--- highRes.py
+++ highRes.py
@@ -1,9 +1,14 @@
 from PIL import Image
 import numpy as np
-img = np.asarray(Image.open('/home/navaneeth/work/test/mine.jpg')) # image to numpy array
+import matplotlib.pyplot as plt
+img = np.array(Image.open('/content/mine.jpg')) # image to numpy array
 img.setflags(write=1) # change read-only to write
-img2 = np.asarray(Image.open('/home/navaneeth/work/test/test2.png'))
+img2 = np.asarray(Image.open('/content/test2.png'))
 img2.setflags(write=1)
-img2[1,0] = img[1,0]
-arr = Image.fromarray(img2)
-arr.save('save.jpg')
+arr = Image.fromarray(img)
+face = img[0:1, 0:1]
+print(face)
+arr2img = Image.fromarray(face2m)
+plt.imshow(arr2img)
+plt.show()
+arr2img.save('save.jpg')
