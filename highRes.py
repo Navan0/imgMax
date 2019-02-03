35a1a67bcd0fea0d06a037139af59eb1b69128e7
Your Name <nktclt@gmail.com>
2019-02-04 01:46:53 +0530

    update repo

--- highRes.py
+++ highRes.py
@@ -1,14 +1,25 @@
+from google.colab import files
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-img = np.array(Image.open('/content/mine.jpg')) # image to numpy array
-img.setflags(write=1) # change read-only to write
+img = np.array(Image.open('/content/mine.jpg'))
+img.setflags(write=1)
 img2 = np.asarray(Image.open('/content/test2.png'))
 img2.setflags(write=1)
-arr = Image.fromarray(img)
-face = img[0:1, 0:1]
-print(face)
-arr2img = Image.fromarray(face2m)
-plt.imshow(arr2img)
+facearr = img[0:1, 0:1]
+face = Image.fromarray(facearr)
+plt.imshow(face)
 plt.show()
-arr2img.save('save.jpg')
+shape = img.shape
+h = shape[0]*4
+w = shape[1]*4
+c=np.zeros([h,w,3],dtype=np.uint8)
+for i in 
+c[0:4,0:4] = facearr
+c = Image.fromarray(c)
+print(facearr)
+print(facearr.shape)
+plt.imshow(c)
+plt.show()
+outimg = c.save('out.jpg')
+files.download('out.jpg')
